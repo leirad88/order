929d68d158bff25f2c9169cb7a9cb3a6ee72f6dd
leirad88 <leirad8@icloud.com>
2024-05-11 01:38:56 -0400

    new2

--- Program.cs
+++ Program.cs
@@ -50,7 +50,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 app.UseMiddleware<ReverseProxyMiddleware>();
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
