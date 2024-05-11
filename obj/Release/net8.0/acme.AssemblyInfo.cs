917182ed0be6069f001f236d59bf7ef9462afe11
leirad88 <leirad8@icloud.com>
2024-05-11 00:04:20 -0400

    vch

--- obj/Release/net8.0/acme.AssemblyInfo.cs
+++ obj/Release/net8.0/acme.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("acme")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+295157e5d7c489cb348218c38b8b50ad48477368")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94605199125adefe3c3a31a35248be08a5a746f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("acme")]
 [assembly: System.Reflection.AssemblyTitleAttribute("acme")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
