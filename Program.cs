71a3d25d076f487c085cf363a2ff1a33a12e275f
leirad88 <leirad8@icloud.com>
2024-05-11 23:06:11 -0400

    new

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Security.Cryptography;
 using System.Text.Json;
 using acme.Data;
@@ -30,6 +31,13 @@ builder.Services
 .WithAccessToken(options =>
 {
     options.Audience = builder.Configuration["Auth0:Audience"];
+});
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.Listen(IPAddress.Any, 5001, listenOptions =>
+    {
+        listenOptions.UseHttps("/etc/letsencrypt/live/5starhealth.care/certificate.pfx", "Sirius880108");
+    });
 });
 builder.Services.Configure<ForwardedHeadersOptions>(options =>
 {
--- obj/Release/net8.0/acme.AssemblyInfo.cs
+++ obj/Release/net8.0/acme.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("acme")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1478967807b80714b0b6c98470ba84144b38e28a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb5ba0a0863ae3f92c50146150874a03a5541fa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("acme")]
 [assembly: System.Reflection.AssemblyTitleAttribute("acme")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
