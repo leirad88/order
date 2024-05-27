66f4e2dc6404eac23df519e17f6e12b023120c3e
leirad88 <leirad8@icloud.com>
2024-05-26 21:19:42 -0400

    new

--- obj/Release/net8.0/acme.AssemblyInfo.cs
+++ obj/Release/net8.0/acme.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("acme")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4a4dcf2f3cf795e79d8abc0104669c0320fd954")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02a3a41f6bcfed6f3707c180983702841d4c96f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("acme")]
 [assembly: System.Reflection.AssemblyTitleAttribute("acme")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
